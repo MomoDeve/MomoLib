486a3ae6a06f5635803798c6030a776c1d415f52
#Momo <40578274+MomoDeve@users.noreply.github.com>
2019-02-27 23:24:53 +0300

    Update time.cpp

--- source/time.cpp
+++ source/time.cpp
@@ -11,17 +11,6 @@
 
 namespace momo
 {
-	namespace duration
-	{
-		enum
-		{
-			nanoseconds,
-			microseconds,
-			seconds,
-			minutes
-		};
-	}
-
 	__time64_t get_ctime()
 	{
 		auto t = std::chrono::system_clock::now();
@@ -50,26 +39,5 @@ namespace momo
 		std::ostringstream oss;
 		oss << std::put_time(std::localtime(&time), "%Y-%m-%d");
 		return oss.str();
-	}
-
-	template<typename T>
-	long long functionTest(T(*f)(), int time = duration::nanoseconds)
-	{
-		auto start = steady_clock::now();
-		f();
-		auto end = steady_clock::now();
-		switch (time)
-		{
-		case duration::minutes:
-			return (long long)std::chrono::duration_cast<std::chrono::minutes>(end - start).count();
-		case duration::seconds:
-			return (long long)std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
-		case duration::microseconds:
-			return (long long)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		case duration::nanoseconds:
-			return (long long)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-		default:
-			return (long long)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-		}
 	}
 }
