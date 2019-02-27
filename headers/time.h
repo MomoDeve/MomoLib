3d51f2e7102054595ea8107e8ce5043f6be8e4ac
#Momo <40578274+MomoDeve@users.noreply.github.com>
2019-02-27 23:26:00 +0300

    Update time.h

--- headers/time.h
+++ headers/time.h
@@ -8,6 +8,17 @@
 
 namespace momo
 {
+	namespace duration
+	{
+		enum
+		{
+			nanoseconds,
+			microseconds,
+			seconds,
+			minutes
+		};
+	}
+
 	__time64_t get_ctime();
 
 	std::string getDate();
@@ -17,5 +28,23 @@ namespace momo
 	std::string getDay();
 
 	template<typename T>
-	long long functionTest(T(*)(), int);
+	long long functionTest(T(*f)(), int time = duration::nanoseconds)
+	{
+		auto start = steady_clock::now();
+		f();
+		auto end = steady_clock::now();
+		switch (time)
+		{
+		case duration::minutes:
+			return (long long)std::chrono::duration_cast<std::chrono::minutes>(end - start).count();
+		case duration::seconds:
+			return (long long)std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+		case duration::microseconds:
+			return (long long)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		case duration::nanoseconds:
+			return (long long)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+		default:
+			return (long long)std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+		}
+	}
 }
